16aeeddd2b6d7125ff63c161564378e5b95834aa
ehacinom <ehacinom@gmail.com>
2014-10-14 13:12:07 -0700

    cleanup

--- test.py
+++
@@ -1,6 +0,0 @@
-def repeatcmd(name, var1, val1, var2, val2):
-    '''Return string value'''
-    cmd = 'SELECT COUNT(*) FROM %s WHERE conversion = 0 and %s = %s'
-    cmd = cmd % (name, var1, val1, var2, val2)
-    cur.execute(cmd)
-    return str(cur.fetchone()[0])
